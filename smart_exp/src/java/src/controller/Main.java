87780e61a86059b0a5e97d568fc8354821e422ee
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2020-11-13 23:32:59 +0100

    updated fazz

--- smart_exp/src/java/src/controller/Main.java
+++ smart_exp/src/java/src/controller/Main.java
@@ -3,7 +3,7 @@ package controller;
 import view.GraphImpl;
 
 public class Main {
-
+	
 	public static void main(String[] args) {
 		new GraphImpl();
 	}
--- smart_exp/src/java/src/view/GraphImpl.java
+++ smart_exp/src/java/src/view/GraphImpl.java
@@ -1,41 +1,65 @@
 package view;
 
-import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.LayoutManager;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.xy.XYDataset;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.chart.plot.Plot;
+import org.jfree.chart.plot.XYPlot;
+
 
 public class GraphImpl implements Graph {
 
 	private final JFrame frame;
 	private final JPanel canvas;
 	
-
-	private final JLabel label;
+	private final JFreeChart positionChart;
+	private final JFreeChart speedChart;
+	private final JFreeChart accelerationChart;
+	
+	private final Plot positionPlot;
+	private final Plot speedPlot;
+	private final Plot accelerationPlot;
+	
+	private final ChartPanel positionPanel;
+	private final ChartPanel speedPanel;
+	private final ChartPanel accelerationPanel;
 	
 	public GraphImpl(){
 
-		this.label = new JLabel("hello");
 		this.frame = new JFrame();
 		this.canvas = new JPanel();
 		
-		this.canvas.add(label);
-		this.frame.add(canvas);
+		this.positionPlot = new XYPlot();
+		this.positionChart = new JFreeChart(positionPlot);
+		this.positionPanel = new ChartPanel(positionChart);
+		
+		this.speedPlot = new XYPlot();
+		this.speedChart = new JFreeChart(speedPlot);
+		this.speedPanel = new ChartPanel(speedChart);
+		
+		this.accelerationPlot = new XYPlot();
+		this.accelerationChart = new JFreeChart(accelerationPlot);
+		this.accelerationPanel = new ChartPanel(accelerationChart);
+		
+		
+		this.canvas.add(positionPanel);
+		this.canvas.add(speedPanel);
+		this.canvas.add(accelerationPanel);
         
+		
+		this.frame.add(canvas);
+		
 		this.frame.pack();
 		this.frame.setVisible(true);
 	}
 	
-
 	@Override
 	public void plot() {
 		
@@ -46,5 +70,4 @@ public class GraphImpl implements Graph {
 		// TODO Auto-generated method stub
 
 	}
-
 }
