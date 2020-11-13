afd1719d6e13b151e25041644d0ad504cb661dbb
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2020-11-13 21:15:13 +0100

    added JFreeChart library for making charts in Java

---
+++ smart_exp/src/java/src/controller/Main.java
@@ -0,0 +1,11 @@
+package controller;
+
+import view.GraphImpl;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new GraphImpl();
+	}
+
+}
--- smart_exp/src/java/src/view/GraphImpl.java
+++ smart_exp/src/java/src/view/GraphImpl.java
@@ -1,11 +1,44 @@
 package view;
 
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
 public class GraphImpl implements Graph {
+
+	private final JFrame frame;
+	private final JPanel canvas;
+	
+
+	private final JLabel label;
+	
+	public GraphImpl(){
+
+		this.label = new JLabel("hello");
+		this.frame = new JFrame();
+		this.canvas = new JPanel();
+		
+		this.canvas.add(label);
+		this.frame.add(canvas);
+        
+		this.frame.pack();
+		this.frame.setVisible(true);
+	}
+	
 
 	@Override
 	public void plot() {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
