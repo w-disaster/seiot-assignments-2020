f8021faa8494f28ec8410d192f060da97fbb3ef9
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2020-11-15 14:49:02 +0100

    changed pins for the buttons

--- smart_exp/src/arduino/smart_exp/LedImpl.cpp
+++ smart_exp/src/arduino/smart_exp/LedImpl.cpp
@@ -1,20 +1,22 @@
 #include "LedImpl.h"
 #include "Arduino.h"
 
-LedImpl::LedImpl(int pin){
+#define BLINK_DELAY
+
+LedImpl::LedImpl(int pin) {
   this->pin = pin;
   pinMode(pin, OUTPUT);
 }
 
-void LedImpl::turnOn(){
+void LedImpl::turnOn() {
   digitalWrite(this->pin, HIGH);
 }
 
 
-void LedImpl::turnOff(){
+void LedImpl::turnOff() {
   digitalWrite(this->pin, LOW);
 }
 
-void LedImpl::blinkLed(int seconds){
-
+void LedImpl::blinkLed(int seconds) {
+  
 }
