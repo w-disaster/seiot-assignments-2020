91f4e12ed936893e646f1cf2de21d3fbcd2619dd
w-disaster <luca.fabri1999@gmail.com>
2021-02-13 22:53:17 +0100

    build send logic with timer interrupt, must write blink ISR correctly and fix sending json format

--- smart_dam/src/remote_hydrometer/lib.cpp
+++ smart_dam/src/remote_hydrometer/lib.cpp
@@ -6,60 +6,40 @@ const double vs = 331.45 + 0.62*20;
 volatile boolean statusLed = false;
 volatile uint32_t lastMillis = 0;
 
-String state;
-volatile float distance;
+float distance;
 unsigned long timestamp;
-volatile boolean msgReady = false;
+boolean msgReady = false;
+boolean isStateChanged; 
+
+State state;
+State precState;
 
 char* ssidName = "Vodafonefabri";
 char* pwd = "3Com2007";
 String address = "http://192.168.1.5:8080";
 
-ESP8266Timer ITimer;
-
-/*
-void sendData(){
-  DynamicJsonDocument data(100);
-  HTTPClient http;
-  http.begin(address + "/api/data");      
-  http.addHeader("Content-Type", "plain/text"); 
-
-  RiverState::State state = riverState->getState();
-  float distance = measurement->getDistance();
-  long timestamp = measurement->getTimestamp();
-  
-  data["State"] = state;
-
-  if(state == RiverState::State::NORMAL){
-    data["Distance"] = distance;
-    data["Timestamp"] = timestamp;
-  } else {
-    data["Distance"] = "";
-    data["Timestamp"] = "";
-  }
-
-  String json = "";
-  serializeJson(data, json);
-  
-  int retCode = http.POST(json);   
-  http.end();
-  msgReady = false;
-}*/
-
-
-void ICACHE_RAM_ATTR readDistanceAndSetState(){
+void readDistanceAndSetState(){
   /* Distance read from HC-SR04 and measurement fields fill*/
   distance = getDistance();
-
+  Serial.println(String("distance: ") + distance);
   /* We determine the State by the distance sampled */
   if(distance > D1_IN_M){
-    state = "NORMAL";
-  } else if(distance >= D1_IN_M && distance < D2_IN_M){
-    state = "PRE_ALARM";
+    state = State::NORMAL;
+  } else if(distance >= D2_IN_M && distance <= D1_IN_M){
+    state = State::PRE_ALARM;
   } else {
-    state = "ALARM";
+    state = State::ALARM;
   }
-  
+  if(precState != state){
+    isStateChanged = true;
+  } else {
+    isStateChanged = false;
+  }
+  precState = state;
+}
+
+void setMsgReady(){
+  Serial.println("setting msg ready");
   /* The data is ready to be sent */
   msgReady = true;
 }
@@ -80,7 +60,6 @@ float getDistance(){
     return d;
 }
 
-
 void ICACHE_RAM_ATTR blinkLed() {
   static bool started = false;
 
@@ -92,9 +71,4 @@ void ICACHE_RAM_ATTR blinkLed() {
 
   digitalWrite(BUILTIN_LED, statusLed);  //Toggle LED Pin
   statusLed = !statusLed;
-
-#if (TIMER_INTERRUPT_DEBUG > 0)
-  Serial.println("Delta ms = " + String(millis() - lastMillis));
-  lastMillis = millis();
-#endif
 }
